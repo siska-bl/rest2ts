8e7d06bdcd5d453dadcdbabacb67fa567da79ada
Lukáš Šiška <lukas.siska@blogic.cz>
2024-06-04 00:46:23 +0200

    Add support for nested properties inside allOf attribute

--- packages/core/src/tests/generate.test.ts
+++ packages/core/src/tests/generate.test.ts
@@ -2,7 +2,7 @@ import SwaggerParser from "@apidevtools/swagger-parser";
 import { describe, expect, test } from "vitest";
 import { generate } from "../lib/generators";
 
-const cases = ["petstore", "nswag", "anonymous_object"];
+const cases = ["petstore", "nswag", "anonymous_object", "allof_nested_properties"];
 
 function fixturePath(name: string) {
   return `./src/tests/fixtures/${name}.json`;
